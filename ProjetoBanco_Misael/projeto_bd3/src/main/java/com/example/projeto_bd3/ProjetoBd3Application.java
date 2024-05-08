df43ea121db5e4770854b12243e45746a177144f
Misael Alexandrino <151184035+MisaelAlexandrino264@users.noreply.github.com>
2024-05-08 11:17:48 -0300

    aula BD3

---
+++ ProjetoBanco_Misael/projeto_bd3/src/main/java/com/example/projeto_bd3/ProjetoBd3Application.java
@@ -0,0 +1,13 @@
+package com.example.projeto_bd3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoBd3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoBd3Application.class, args);
+	}
+
+}
---
+++ ProjetoBanco_Misael/projeto_bd3/src/test/java/com/example/projeto_bd3/ProjetoBd3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projeto_bd3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoBd3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
